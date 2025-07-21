eb13467f17f4de6f4966909d4b0ede0c6ad933ec
Umarfarukh Shaikh <umarfarukh.shaikh@sumasoft.net>
2025-07-21 18:21:01 +0530

    programe 8 regarding sum of digit number

---
+++ C# All Practical Exaple/9.SumOfDigitNo/Program.cs
@@ -0,0 +1,14 @@
+﻿// Sum of Digits Program in C#
+
+Console.WriteLine("Enter a number =");
+int n = Int32.Parse(Console.ReadLine());
+int sum = 0;
+while (n!=0)
+{
+    int lastDigit = n % 10;
+    sum = sum + lastDigit;
+    n = n / 10;
+}
+Console.WriteLine($"Sum of digit is {sum}");
+
+Console.ReadKey();
