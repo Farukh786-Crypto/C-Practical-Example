1396004699ea4c59cdd50156df75e68b46823c6c
Umarfarukh Shaikh <umarfarukh.shaikh@sumasoft.net>
2025-07-21 16:18:36 +0530

    program 6  is added

---
+++ C# All Practical Exaple/6.NoDiviBy2/Program.cs
@@ -0,0 +1,15 @@
+﻿// C# Program to Check if a Number is Divisible by 2
+
+Console.WriteLine("Enter a Number =");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.WriteLine($"{n} is divisible by 2");
+}
+else
+{
+    Console.WriteLine($"{n} is not divisible by 2");
+}
+
+Console.ReadKey();
