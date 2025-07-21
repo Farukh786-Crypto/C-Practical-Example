20537d620c3fc1513b2b6f6fb47bd6822d0c9899
Umarfarukh Shaikh <umarfarukh.shaikh@sumasoft.net>
2025-07-21 18:00:58 +0530

    program 6 and 7 added

---
+++ C# All Practical Exaple/7.SumOfMultipleNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// C# Program to Find the Sum of All the Multiples of 3 and 5
+int sum = 0;
+for (int i = 0; i <= 10; i++)
+{
+    int a = i % 3;
+    int b = i % 5;
+    if (a==0 || b==0)
+    {
+        sum = sum  + i;
+    }
+}
+Console.WriteLine($" Sum is {sum}");
+Console.ReadKey();
---
+++ C# All Practical Exaple/8.Multipleof17/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Enter the size of an array =");
+int n = Int32.Parse(Console.ReadLine());
+
+int[] num = new int[n];
+
+for (int i = 1; i <= n; i++)
+{
+    int a = i % 17;
+    if (a == 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
+Console.ReadKey();
