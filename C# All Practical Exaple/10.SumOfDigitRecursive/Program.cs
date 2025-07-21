c7049f37f7696dbd4e8ee09d03894c73e2054a31
Umarfarukh Shaikh <umarfarukh.shaikh@sumasoft.net>
2025-07-21 18:36:41 +0530

    sum of didit recursive code added in program 10

---
+++ C# All Practical Exaple/10.SumOfDigitRecursive/Program.cs
@@ -0,0 +1,19 @@
+﻿
+static int SumOfDigits(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    return (num%10) + SumOfDigits(num / 10);
+}
+
+
+Console.WriteLine("Enter Number");
+int n= Int32.Parse(Console.ReadLine());
+
+int result  = SumOfDigits(n);
+
+Console.WriteLine($"Sum of digit :{result}");
+
+Console.ReadKey();
