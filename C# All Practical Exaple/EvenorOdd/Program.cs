b1e26276fdc59df22b6e4586901787d99523a37c
Umarfarukh Shaikh <umarfarukh.shaikh@sumasoft.net>
2025-07-21 13:28:14 +0530

    add code

---
+++ C# All Practical Exaple/EvenorOdd/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Channels;
+
+
+Console.WriteLine("Enter a Number =:");
+int n = Int32.Parse(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.WriteLine($"{n} is Even Number");
+}
+else
+{
+    Console.WriteLine($"{n} is Odd Number");
+}
+Console.ReadKey();
+
