318e9383939c7e5c6f99cc99a598935130438b93
Umarfarukh Shaikh <umarfarukh.shaikh@sumasoft.net>
2025-07-24 17:05:57 +0530

    chech character

---
+++ C# All Practical Exaple/18.CheckConditionLowercase/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter a Character From A to C");
+char ch = Convert.ToChar(Console.ReadLine());
+Console.WriteLine();
+
+char lowerchar = Char.ToLower(ch);
+
+if (lowerchar == 'a')
+{
+    Console.WriteLine("You choose Option A");
+}
+if (lowerchar == 'b')
+{
+    Console.WriteLine("You choose Option B");
+}
+if (lowerchar == 'c')
+{
+    Console.WriteLine("You choose Option C");
+}
+Console.ReadKey();
