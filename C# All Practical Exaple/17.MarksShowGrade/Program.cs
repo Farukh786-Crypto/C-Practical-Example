511a66c9eea5dc454dac433000480617b3e42bf3
Umarfarukh Shaikh <umarfarukh.shaikh@sumasoft.net>
2025-07-24 17:00:03 +0530

    marks show grade programe in c#

---
+++ C# All Practical Exaple/17.MarksShowGrade/Program.cs
@@ -0,0 +1,27 @@
+﻿// C# Program to Read a Grade and Display the Equivalent Description
+
+Console.WriteLine("Enter the Grade in Uppercase");
+char grade = Convert.ToChar(Console.ReadLine());
+
+switch (grade)
+{
+	case 'S':
+		Console.WriteLine("Super");
+		break;
+	case 'A':
+		Console.WriteLine("Very Good");
+		break;
+	case 'B':
+		Console.WriteLine("FAIR");
+		break;
+	case 'y':
+		Console.WriteLine("FAIL");
+		break;
+	default:
+        Console.WriteLine("Error in Grade \n");
+		break;
+		Console.ReadLine();
+}
+
+
+Console.ReadKey();
