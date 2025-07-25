96d19eab579bee890b5c1d80240269842cdb98fb
Umarfarukh Shaikh <umarfarukh.shaikh@sumasoft.net>
2025-07-25 11:49:32 +0530

    add boxing and unBoxing Logic

---
+++ C# All Practical Exaple/04.clsBoxing/Program.cs
@@ -0,0 +1,15 @@
+﻿// C# Program to Demonstrate Boxing Operations
+
+// Here Boxing is the process of converting a value type to the type object or
+// to any interface type implemented by this value type.
+
+int num = 100;
+object obj = num; // Boxing: converting int to object
+
+int unBoxingnum = (int)obj; // Unboxing: converting object back to value type
+
+Console.WriteLine("Original Number :"+num);
+Console.WriteLine("Boxing Object :"+obj);
+Console.WriteLine("UnBoxing Number :"+unBoxingnum);
+
+Console.ReadKey(); // Wait for user input before closing the console window
