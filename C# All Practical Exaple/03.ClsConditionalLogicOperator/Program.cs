16678e57a522d7d492ada91f0a9a28419281831c
Umarfarukh Shaikh <umarfarukh.shaikh@sumasoft.net>
2025-07-25 11:08:45 +0530

    age valid condition check

---
+++ C# All Practical Exaple/03.ClsConditionalLogicOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿// C# Program to Illustrate the use of Conditional Logical Operator
+
+int Age;
+Console.WriteLine("Enter the Age =");
+Age = Int32.Parse(Console.ReadLine());
+
+bool isAgeValid = (Age > 18) ? true : false;
+if (isAgeValid)
+{
+
+    Console.WriteLine($"Age is Valid For Vote");
+}
+else
+    Console.WriteLine($"Age is Invalid For Vote");
+
+    Console.ReadKey();
