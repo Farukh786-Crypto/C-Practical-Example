92c787dadf149302fa381b95ab91a7cf93d8e070
Umarfarukh Shaikh <umarfarukh.shaikh@sumasoft.net>
2025-07-25 11:02:08 +0530

    add min and max datatype

---
+++ C# All Practical Exaple/01.ClsMaxRangeDatatype/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+Console.WriteLine("The minimum Range of the Decimal Data =" + Decimal.MinValue);
+Console.WriteLine("The minimum Range of Float Data =" + float.MinValue);
+Console.WriteLine("The minimum Range of Double Data =" + Double.MinValue);
+Console.WriteLine("The minimum Range of Int Data =" + int.MinValue);
+Console.ReadKey();
---
+++ C# All Practical Exaple/02.ClsMinRangeDataType/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+Console.WriteLine("The Maximum Range of the Decimal Data =" + Decimal.MaxValue);
+Console.WriteLine("The Maximum Range of Float Data =" + float.MaxValue);
+Console.WriteLine("The Maximum Range of Double Data =" + Double.MaxValue);
+Console.WriteLine("The Maximum Range of Int Data =" + int.MaxValue);
+Console.ReadKey();
